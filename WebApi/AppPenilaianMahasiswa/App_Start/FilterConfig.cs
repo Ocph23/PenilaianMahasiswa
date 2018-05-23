69d6ea1027be89e2ebd23d8531c274463ade2cb0
Yoseph Kungkung <ocph23.develop@hotmail.com>
2018-05-23 21:20:28 +0700

    #Create Project

---
+++ WebApi/AppPenilaianMahasiswa/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AppPenilaianMahasiswa
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
